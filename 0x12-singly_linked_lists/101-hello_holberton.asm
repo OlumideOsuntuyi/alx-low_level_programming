481976d7ca9f68a08db2ee56f3dc37985397772d
OlumideOsuntuyi <olugeorge61@gmail.com>
2023-08-24 11:23:33 -0700

    making checker happy

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,10 @@
-section .data
-    format db "Hello, Holberton", 0
-
-section .text
-    global main
-    extern printf
-
+          global    main
+          extern    printf
 main:
-    mov edi, format     ; Load the format string address
-    xor eax, eax        ; Clear eax register
-    call printf        ; Call the printf function
-    mov eax, 0          ; Set the return value to 0
-    ret                 ; Return from the main function
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
